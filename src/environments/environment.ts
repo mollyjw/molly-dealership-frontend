f2872833ac51277a7d0464119e592064bda8875d
MollyJW <mollyjwhitener@gmail.com>
2020-10-06 19:37:20 -0500

    trying to fix cors error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://cors-anywhere.herokuapp.com/https://vehicle-dealership.herokuapp.com/'
+  apiUrl: 'https://vehicle-dealership.herokuapp.com/'
 };
 
 /*
