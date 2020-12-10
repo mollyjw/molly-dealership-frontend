8379828209bfb3d3feaca313502e73554e50e33a
MollyJW <mollyjwhitener@gmail.com>
2020-12-09 20:36:02 -0600

    modified server.js

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ app.get('/*', (req,res)=>{
     res.sendFile(path.join(__dirname,'/dist/wheely-good-cars/index.html'));
 });
 
-app.listen(process.env.PORT || 8080, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Server started')
 })
+
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
 
